11baf9b07970c79adee36b361387f5ba5005c350
unknown <811706959@qq.com>
2019-09-11 22:05:57 +0800

    新增tododo

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import TodoItem from './TodoItem';
     constructor(props){
       super(props)
       this.state = {
-        newTodo:"test",
+        newTodo: '',
         todoList:[
           {id:1,title:'第一个待办'},
           {id:2,title:'第二个待办'}
@@ -21,7 +21,7 @@ import TodoItem from './TodoItem';
        
       let todos = this.state.todoList.map((item,index)=>{
         return(
-          <li>
+          <li key ={index}>
             <TodoItem todo={item} />
           </li>
         )
@@ -30,7 +30,7 @@ import TodoItem from './TodoItem';
        <div className="App">
          <h1>我的待办</h1>
          <div className="inputWrapper">
-         <TodoInput content={this.state.newTodo} onSubmit={this.addTodo} />
+         <TodoInput content={this.state.newTodo} onSubmit={this.addTodo.bind(this)} />
          </div>
          <ol>
            {todos}
@@ -38,9 +38,26 @@ import TodoItem from './TodoItem';
        </div>
      )
     }
-    addTodo(){
-      console.log('我得添加一个todo了')
+    addTodo(event){
+      this.state.todoList.push({
+        id: idMaker(),
+        title: event.target.value,
+        status: null,
+        deleted: false
+      })
+      this.setState({
+        newTodo: '',
+        todoList: this.state.todoList
+      })
     }
   }
 
 export default App;
+
+
+let id = 0
+
+function idMaker(){
+  id += 1
+  return id
+}
