b5f61cb6063026f7e8a8f2a4ceef64876ce886ce
unknown <811706959@qq.com>
2019-09-11 14:30:05 +0800

    App 传一个函数给TodoInputut

--- src/App.js
+++ src/App.js
@@ -30,13 +30,16 @@ import TodoItem from './TodoItem';
        <div className="App">
          <h1>我的待办</h1>
          <div className="inputWrapper">
-         <TodoInput content={this.state.newTodo} />
+         <TodoInput content={this.state.newTodo} onSubmit={this.addTodo} />
          </div>
          <ol>
            {todos}
          </ol>
        </div>
      )
+    }
+    addTodo(){
+      console.log('我得添加一个todo了')
     }
   }
 
