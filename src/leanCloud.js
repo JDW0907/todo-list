124ea794ad5471b03e95a371c0022f0567a79224
unknown <811706959@qq.com>
2019-09-17 14:50:19 +0800

    添加UserDialog.css

---
+++ src/leanCloud.js
@@ -0,0 +1,10 @@
+import AV from 'leancloud-storage'
+
+var APP_ID = "9Mb0BdKRJtWvdKwNCw2tM8wM-gzGzoHsz"
+var APP_KEY = "5Y5a7OL5VFy8MpaTdd31UCFG"
+AV.init({
+  appId: APP_ID,
+  appKey: APP_KEY
+})
+
+export default AV
