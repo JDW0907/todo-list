fbcc2816a8141312c46efcdb5ca35cc7c8efb169
unknown <811706959@qq.com>
2019-09-17 14:20:21 +0800

    删除leancloud测试代代码

--- src/App.js
+++ src/App.js
@@ -14,13 +14,7 @@ AV.init({
   appKey: APP_KEY
 })
 
-var TestObject = AV.Object.extend('TestObject')
-var testObject = new TestObject()
-testObject.save({
-  words: 'Hello World!'
-}).then(function(object) {
-  alert('LeanCloud Rocks!')
-})
+
 
 
     class App extends Component {
