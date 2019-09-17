c82406db2f3675c207adde6eeb691585722cab66
unknown <811706959@qq.com>
2019-09-17 14:29:16 +0800

    新建leanCloud.js

--- src/App.js
+++ src/App.js
@@ -7,13 +7,6 @@ import TodoItem from './TodoItem';
 import * as localStore from './localStore';
 import AV from 'leancloud-storage';
 
-var APP_ID = "9Mb0BdKRJtWvdKwNCw2tM8wM-gzGzoHsz"
-var APP_KEY = "5Y5a7OL5VFy8MpaTdd31UCFG"
-AV.init({
-  appId: APP_ID,
-  appKey: APP_KEY
-})
-
 
 
 
