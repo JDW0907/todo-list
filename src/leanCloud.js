cf8bafb116f15daf82483d07a6019abb978d4383
unknown <811706959@qq.com>
2019-09-24 14:30:50 +0800

    fix bug

--- src/leanCloud.js
+++ src/leanCloud.js
@@ -33,6 +33,8 @@ export const TodoModel = {
     let acl = new AV.ACL()
     acl.setPublicReadAccess(false) // 注意这里是 false
     acl.setWriteAccess(AV.User.current(), true)
+    acl.setReadAccess(AV.User.current(), true)
+
 
     todo.setACL(acl);
     todo.save().then(function (response) {
