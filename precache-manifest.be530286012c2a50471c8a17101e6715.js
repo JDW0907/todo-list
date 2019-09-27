d7013cb11baa5f3756bd62d419c2ea1df313a011
John.JIANG <43057514+JDW0907@users.noreply.github.com>
2019-09-27 18:01:56 +0800

    fix bug

---
+++ precache-manifest.be530286012c2a50471c8a17101e6715.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "967bff65c2def4865967dcb9e6108783",
+    "url": "/Todo-List/build/static/media/login.967bff65.jpg"
+  },
+  {
+    "revision": "f732030e5d7920bf7e9a",
+    "url": "/Todo-List/build/static/js/runtime~main.f732030e.js"
+  },
+  {
+    "revision": "bcf912b0e6b1edb61996",
+    "url": "/Todo-List/build/static/js/main.bcf912b0.chunk.js"
+  },
+  {
+    "revision": "3a2d9f24f5d33e6e56cb",
+    "url": "/Todo-List/build/static/js/1.3a2d9f24.chunk.js"
+  },
+  {
+    "revision": "bcf912b0e6b1edb61996",
+    "url": "/Todo-List/build/static/css/main.abb29948.chunk.css"
+  },
+  {
+    "revision": "3a2d9f24f5d33e6e56cb",
+    "url": "/Todo-List/build/static/css/1.2bbef261.chunk.css"
+  },
+  {
+    "revision": "0edfee44eaa2478a17786be9602afeeb",
+    "url": "/Todo-List/build/index.html"
+  }
+];
